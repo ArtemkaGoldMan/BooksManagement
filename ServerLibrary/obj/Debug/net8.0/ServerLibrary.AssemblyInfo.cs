abcf908e84173085f8a1d542bb6d1b405042b2b6
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2024-12-09 12:42:07 +0100

    full v1

--- ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
+++ ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2394d8ccd0a4b45bf58d2d26c80ba99e97c1dbef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c9b68f6f315b2f0530fd6bd046adb3fa0d2d82e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
